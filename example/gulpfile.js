11558d8d38e6095c09b006be1bb670ee0f8aa782
Steve Brandt <steve.brandt@springer.com>
2017-03-26 21:28:08 +0200

    options and selector without '.'

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -2,9 +2,14 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     cssScala = require('../index.js');
 
+var options = {
+    className:'Css',
+    packageName:'com.example.css'
+};
+
 gulp.task('default', function() {
     gulp.src('foo.css').
-    pipe(cssScala('Css', 'com.example.css')).
+    pipe(cssScala(options)).
     pipe(rename('Css.scala')).
     pipe(gulp.dest('dest'));
 });

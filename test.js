d770e73477e31778182b52579fd496d13f4d5d5b
Steve Brandt <steve.brandt@springer.com>
2017-03-30 22:13:53 +0200

    basic test setup

---
+++ test.js
@@ -0,0 +1,11 @@
+var assert = require('assert'),
+ es = require('event-stream'),
+ File = require('vinyl');
+
+
+describe('gulp-css-scala', function() {
+    it('should be true', function(done) {
+        true;
+        done();
+    });
+});
